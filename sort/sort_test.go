e466f507a173112955de911addcf34028eeb0db2
LiHS <lihsai0@gmail.com>
2022-01-04 22:42:24 +0800

    [UPDATE] sort_test.go for all sort functions

--- sort/sort_test.go
+++ sort/sort_test.go
@@ -2,19 +2,36 @@ package sort
 
 import (
 	"reflect"
+	"runtime"
 	"testing"
 )
 
+var functionsToTest = []func(arr []int){
+	BubbleSort,
+	InsertSort,
+	SelectSort,
+	MergeSort,
+	QuickSort2Ways,
+	QuickSort3Ways,
+}
+
 func Test(t *testing.T) {
+	for _, fn := range functionsToTest {
+		runSort(fn, t)
+	}
+}
+
+func runSort(fn func(arr []int), t *testing.T) {
+	fnName := runtime.FuncForPC(reflect.ValueOf(fn).Pointer()).Name()
 	for _, test := range testCases {
 		actual := make([]int, len(test.unsorted))
 		copy(actual, test.unsorted)
-		QuickSort3Ways(actual)
+		fn(actual)
 		if reflect.DeepEqual(actual, test.sorted) {
-			t.Logf("PASS: %s", test.massage)
+			t.Logf("PASS: %s %s", fnName, test.massage)
 		} else {
-			t.Errorf("FAIL: %s\nMergeSort(%v)\nExpected: %#v, Actual: %#v",
-				test.massage, test.unsorted, test.sorted, actual)
+			t.Errorf("FAIL: %s\n%s(%v)\nExpected: %#v, Actual: %#v",
+				test.massage, fnName, test.unsorted, test.sorted, actual)
 		}
 	}
 }
